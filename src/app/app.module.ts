d0f48e205ca9a2c77ed580a2029e928447c1841d
ewan questel <ew.questel@gmail.com>
2023-11-07 15:33:20 +0100

    ajout de composants
    composants pour bouton, main title, et "test"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ColorSelectComponent } from './pages/color-select/color-select.component';
 import { RoleComponent } from './pages/role/role.component';
+import { ButtonComponent } from './shared/layout/button/button.component';
+import { MainTitleComponent } from './shared/layout/main-title/main-title.component';
+import { TestComponent } from './shared/test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ColorSelectComponent,
     RoleComponent,
+    ButtonComponent,
+    MainTitleComponent,
+    TestComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/layout/main-title/main-title.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-title',
+  templateUrl: './main-title.component.html',
+  styleUrls: ['./main-title.component.scss']
+})
+export class MainTitleComponent {
+
+}
