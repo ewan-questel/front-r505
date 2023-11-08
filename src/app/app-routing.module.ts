15d6b1e5d7b1bcc3e126c461589d4347518fc7e8
ewan questel <ew.questel@gmail.com>
2023-11-08 12:10:31 +0100

    modification color select
    ajout de fonctionalités pour la page color-select

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ColorSelectComponent } from './pages/color-select/color-select.component';
+import { WaitingRoomComponent } from './pages/waiting-room/waiting-room.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ColorSelectComponent },
+  { path: 'waiting', component: WaitingRoomComponent }
+];
 
 @NgModule({
+  
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
--- src/app/pages/color-select/color-select.component.ts
+++ src/app/pages/color-select/color-select.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+/* import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-color-select',
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class ColorSelectComponent {
 
+} */
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-color-select',
+  templateUrl: './color-select.component.html',
+  styleUrls: ['./color-select.component.scss']
+})
+export class ColorSelectComponent {
+  buttonColors: string[] = ['red', 'green', 'blue', 'yellow', 'orange', 'purple', 'pink', 'cyan', 'gray', 'brown', 'teal', 'indigo', 'lime', 'magenta', 'violet', 'turquoise'];
 }
