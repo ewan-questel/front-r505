060f066e38e7c352b346477c4d47003724600b2a
ewan questel <ew.questel@gmail.com>
2023-11-09 10:35:56 +0100

    modify main title component
    changes and added the main title component to color select and waiting room

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ColorSelectComponent } from './pages/color-select/color-select.component';
 import { WaitingRoomComponent } from './pages/waiting-room/waiting-room.component';
+import { RoleComponent } from './pages/role/role.component';
 
 const routes: Routes = [
   { path: '', component: ColorSelectComponent },
-  { path: 'waiting', component: WaitingRoomComponent }
+  { path: 'waiting', component: WaitingRoomComponent },
+  { path: 'role', component: RoleComponent }
 ];
 
 @NgModule({
--- src/app/pages/color-select/color-select.component.ts
+++ src/app/pages/color-select/color-select.component.ts
@@ -1,15 +1,5 @@
-/* import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-color-select',
-  templateUrl: './color-select.component.html',
-  styleUrls: ['./color-select.component.scss']
-})
-export class ColorSelectComponent {
-
-} */
-
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-color-select',
